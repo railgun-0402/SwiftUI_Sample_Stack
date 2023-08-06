2aa6f9864c461490ca48bd95da6774f5201d1613
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2023-08-06 23:24:42 +0900

    Initial Commit

---
+++ HelloSwiftUI/HelloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUIApp.swift
+//  HelloSwiftUI
+//
+//  Created by 菅原大輝 on 2023/08/06.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
