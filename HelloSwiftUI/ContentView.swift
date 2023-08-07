efb0812d5f7c58fcdaa8ae84c432b904d5c5cb45
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2023-08-07 13:12:43 +0900

    first commit

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -2,19 +2,83 @@
 //  ContentView.swift
 //  HelloSwiftUI
 //
-//  Created by 菅原大輝 on 2023/08/06.
-//
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+        // 1段目
+            ZStack {
+                Rectangle()
+                    .foregroundColor(.yellow)
+                    .frame(width: 300, height: 300)
+                
+                VStack {
+                    HStack {
+                        Rectangle()
+                            .foregroundColor(.black)
+                            .frame(width: 60, height: 20)
+                        
+                        ZStack {
+                            Rectangle()
+                                .foregroundColor(.gray)
+                                .frame(width: 90, height: 90)
+                            Rectangle()
+                                .foregroundColor(.white)
+                                .frame(width: 70, height: 70)
+                            Rectangle()
+                                .foregroundColor(.black)
+                                .frame(width: 20, height: 20)
+                        }
+                        ZStack {
+                            Rectangle()
+                                .foregroundColor(.gray)
+                                .frame(width: 90, height: 90)
+                            Rectangle()
+                                .foregroundColor(.white)
+                                .frame(width: 70, height: 70)
+                            Rectangle()
+                                .foregroundColor(.black)
+                                .frame(width: 20, height: 20)
+                        }
+                        
+                        Rectangle()
+                            .foregroundColor(.black)
+                            .frame(width: 60, height: 20)
+                        
+                    }
+                    
+                    // 2段目
+                    ZStack {
+                        Rectangle()
+                            .foregroundColor(.black)
+                            .frame(width: 70, height: 40)
+                        
+                        VStack {
+                            Rectangle()
+                                .foregroundColor(.white)
+                                .frame(width: 60, height: 10)
+                            Rectangle()
+                                .foregroundColor(.red)
+                                .frame(width: 60, height: 10)
+                            
+                        }
+                    }
+                }
+            }
+            Rectangle()
+                .foregroundColor(.blue)
+                .frame(width: 300, height: 100)
+            HStack {
+                Rectangle()
+                    .foregroundColor(.black)
+                    .frame(width: 70, height: 30)
+                Rectangle()
+                    .foregroundColor(.black)
+                    .frame(width: 70, height: 30)
+            }
+    }
         .padding()
     }
 }
